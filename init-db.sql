1cd8d1423d038f07c83a0628889e96ea31bd4895
liladhar <liladharrrrr@gmail.com>
2026-07-28 01:27:01 +0530

    chore: update MySQL credentials in environment and initialization scripts
    - Modify .env.example to reflect new MySQL root credentials.
    - Update init-db.sql to grant privileges to the root user instead of the previous user.

--- init-db.sql
+++ init-db.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS waste_management CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE DATABASE IF NOT EXISTS central_core_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-GRANT ALL PRIVILEGES ON waste_management.* TO 'user'@'%';
-GRANT ALL PRIVILEGES ON central_core_db.* TO 'user'@'%';
+GRANT ALL PRIVILEGES ON waste_management.* TO 'root'@'%';
+GRANT ALL PRIVILEGES ON central_core_db.* TO 'root'@'%';
 FLUSH PRIVILEGES;
